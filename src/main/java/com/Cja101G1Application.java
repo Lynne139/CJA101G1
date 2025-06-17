f41bcc574ecf0baacfccc7d7e48ffefcbe8bfc92
Lynne <4105033139@smail.nchu.edu.tw>
2025-06-17 14:50:00 +0800

    first update

---
+++ src/main/java/com/Cja101G1Application.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cja101G1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cja101G1Application.class, args);
+	}
+
+}
