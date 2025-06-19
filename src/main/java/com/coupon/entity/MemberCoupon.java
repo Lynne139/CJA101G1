a17d09cfe2c97cb329b70bdfa33b4a15e17f2e7a
CJA101_36 <clearng7531@gmail.com>
2025-06-19 20:37:26 +0800

    Finish Coupon & MemberCoupon Entity

--- src/main/java/com/coupon/entity/MemberCoupon.java
+++ src/main/java/com/coupon/entity/MemberCoupon.java
@@ -1,81 +1,78 @@
-package com.coupon.entity;
-
-import jakarta.persistence.*;
-import java.time.LocalDateTime;
-
-@Entity
-@Table(name = "member_coupon")
-public class MemberCoupon {
-
-    @EmbeddedId
-    private MemberCouponId id;
-
-    @Column(name = "is_used", nullable = false)
-    private Boolean isUsed = false;
-
-    @Column(name = "claim_time", nullable = false, updatable = false, insertable = false)
-    private LocalDateTime claimTime;
-
-    @Column(name = "used_time")
-    private LocalDateTime usedTime;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @MapsId("couponCode")
-    @JoinColumn(name = "coupon_code", referencedColumnName = "coupon_code")
-    private Coupon coupon;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @MapsId("memberId")
-    @JoinColumn(name = "member_id", referencedColumnName = "member_id")
-    private Member member;
-
-    // Getters & Setters
-
-    public MemberCouponId getId() {
-        return id;
-    }
-
-    public void setId(MemberCouponId id) {
-        this.id = id;
-    }
-
-    public Boolean getIsUsed() {
-        return isUsed;
-    }
-
-    public void setIsUsed(Boolean isUsed) {
-        this.isUsed = isUsed;
-    }
-
-    public LocalDateTime getClaimTime() {
-        return claimTime;
-    }
-
-    public void setClaimTime(LocalDateTime claimTime) {
-        this.claimTime = claimTime;
-    }
-
-    public LocalDateTime getUsedTime() {
-        return usedTime;
-    }
-
-    public void setUsedTime(LocalDateTime usedTime) {
-        this.usedTime = usedTime;
-    }
-
-    public Coupon getCoupon() {
-        return coupon;
-    }
-
-    public void setCoupon(Coupon coupon) {
-        this.coupon = coupon;
-    }
-
-    public Member getMember() {
-        return member;
-    }
-
-    public void setMember(Member member) {
-        this.member = member;
-    }
-}
+//package com.coupon.entity;
+//
+//import jakarta.persistence.*;
+//import java.time.LocalDateTime;
+//
+//@Entity
+//@Table(name = "member_coupon")
+//public class MemberCoupon {
+//
+//    @EmbeddedId
+//    private MemberCouponId id;
+//
+//    @Column(name = "is_used", nullable = false)
+//    private Boolean isUsed = false;
+//
+//    // claim_time在DB中已寫DATETIME DEFAULT CURRENT_TIMESTAMP，故insertable = false
+//    @Column(name = "claim_time", nullable = false, updatable = false, insertable = false)
+//    private LocalDateTime claimTime;
+//
+//    @Column(name = "used_time")
+//    private LocalDateTime usedTime;
+//
+//    @ManyToOne(fetch = FetchType.EAGER)
+//    @MapsId("couponCode")
+//    @JoinColumn(name = "coupon_code", referencedColumnName = "coupon_code")
+//    private Coupon coupon;
+//
+//    @ManyToOne(fetch = FetchType.EAGER)
+//    @MapsId("memberId")
+//    @JoinColumn(name = "member_id", referencedColumnName = "member_id")
+//    private Member member;
+//
+//    // Getters & Setters
+//
+//    public MemberCouponId getId() {
+//        return id;
+//    }
+//
+//    public void setId(MemberCouponId id) {
+//        this.id = id;
+//    }
+//
+//    public Boolean getIsUsed() {
+//        return isUsed;
+//    }
+//
+//    public void setIsUsed(Boolean isUsed) {
+//        this.isUsed = isUsed;
+//    }
+//
+//    public LocalDateTime getClaimTime() {
+//        return claimTime;
+//    }
+//
+//    public LocalDateTime getUsedTime() {
+//        return usedTime;
+//    }
+//
+//    public void setUsedTime(LocalDateTime usedTime) {
+//        this.usedTime = usedTime;
+//    }
+//
+//    public Coupon getCoupon() {
+//        return coupon;
+//    }
+//
+//    public void setCoupon(Coupon coupon) {
+//        this.coupon = coupon;
+//    }
+//
+//    public Member getMember() {
+//        return member;
+//    }
+//
+//    public void setMember(Member member) {
+//        this.member = member;
+//    }
+//}
--- src/main/java/com/coupon/entity/MemberCouponId.java
+++ src/main/java/com/coupon/entity/MemberCouponId.java
@@ -1,54 +1,59 @@
-package com.coupon.entity;
-
-import jakarta.persistence.Embeddable;
-import java.io.Serializable;
-import java.util.Objects;
-
-@Embeddable
-public class MemberCouponId implements Serializable {
-
-    private String couponCode;
-    private Integer memberId;
-
-    // Constructors
-    public MemberCouponId() {}
-
-    public MemberCouponId(String couponCode, Integer memberId) {
-        this.couponCode = couponCode;
-        this.memberId = memberId;
-    }
-
-    // Getters & Setters
-
-    public String getCouponCode() {
-        return couponCode;
-    }
-
-    public void setCouponCode(String couponCode) {
-        this.couponCode = couponCode;
-    }
-
-    public Integer getMemberId() {
-        return memberId;
-    }
-
-    public void setMemberId(Integer memberId) {
-        this.memberId = memberId;
-    }
-
-    // equals() & hashCode()
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof MemberCouponId)) return false;
-        MemberCouponId that = (MemberCouponId) o;
-        return Objects.equals(couponCode, that.couponCode) &&
-               Objects.equals(memberId, that.memberId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(couponCode, memberId);
-    }
-}
+//package com.coupon.entity;
+//
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Embeddable;
+//
+//import java.io.Serializable;
+//import java.util.Objects;
+//
+//@Embeddable
+//public class MemberCouponId implements Serializable {
+//
+//    @Column(name = "coupon_code", length = 8, nullable = false)
+//    private String couponCode;
+//
+//    @Column(name = "member_id", nullable = false)
+//    private Integer memberId;
+//
+//    // Constructors
+//    public MemberCouponId() {}
+//
+//    public MemberCouponId(String couponCode, Integer memberId) {
+//        this.couponCode = couponCode;
+//        this.memberId = memberId;
+//    }
+//
+//    // Getters & Setters
+//
+//    public String getCouponCode() {
+//        return couponCode;
+//    }
+//
+//    public void setCouponCode(String couponCode) {
+//        this.couponCode = couponCode;
+//    }
+//
+//    public Integer getMemberId() {
+//        return memberId;
+//    }
+//
+//    public void setMemberId(Integer memberId) {
+//        this.memberId = memberId;
+//    }
+//
+//    // equals() & hashCode()
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (!(o instanceof MemberCouponId)) return false;
+//        MemberCouponId that = (MemberCouponId) o;
+//        return Objects.equals(couponCode, that.couponCode) &&
+//               Objects.equals(memberId, that.memberId);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(couponCode, memberId);
+//    }
+//}
