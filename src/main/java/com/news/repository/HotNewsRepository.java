ac357e37da02019ab70340598f9a0dfa21653e4d
Damon <ro8965418@gmail.com>
2025-06-26 11:11:21 +0800

    news_update2

--- src/main/java/com/news/repository/HotNewsRepository.java
+++ src/main/java/com/news/repository/HotNewsRepository.java
@@ -4,6 +4,9 @@ import com.news.entity.HotNews;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface HotNewsRepository extends JpaRepository<HotNews, Integer> {
+    List<HotNews> findByIsDisplayTrue();
 }
--- src/main/java/com/news/repository/PromotionNewsRepository.java
+++ src/main/java/com/news/repository/PromotionNewsRepository.java
@@ -4,6 +4,9 @@ import com.news.entity.PromotionNews;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PromotionNewsRepository extends JpaRepository<PromotionNews, Integer> {
+    List<PromotionNews> findByIsDisplayTrue();
 } 
