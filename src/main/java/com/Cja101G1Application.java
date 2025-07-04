f5974d832ac2cf3db8538ba6308a315a2ea1d82f
zih_wei <a0917241391@gmail.com>
2025-07-04 11:58:42 +0800

    mysqlpic & member1data update

--- src/main/java/com/Cja101G1Application.java
+++ src/main/java/com/Cja101G1Application.java
@@ -1,8 +1,12 @@
 package com;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.member.model.MemberImageUploader;
 
 @SpringBootApplication
 @EnableScheduling 
@@ -11,5 +15,11 @@ public class Cja101G1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Cja101G1Application.class, args);
 	}
-
+	
+	@Bean
+	public CommandLineRunner run(MemberImageUploader uploader) {
+		return args -> {
+			uploader.uploadAllMemberImages(); // 啟動時塞入會員照片 1~10.png
+		};
+	}
 }
