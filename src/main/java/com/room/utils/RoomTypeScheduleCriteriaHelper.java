a9c6c9b40d5c92b3f6f445f46301bc33200cac53
C60 <lolinouob@gmail.com>
2025-07-09 08:35:09 +0800

    fix

--- src/main/java/com/room/utils/RoomTypeScheduleCriteriaHelper.java
+++ src/main/java/com/room/utils/RoomTypeScheduleCriteriaHelper.java
@@ -9,7 +9,6 @@ import com.roomtypeschedule.model.RoomTypeScheduleVO;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Expression;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
 
